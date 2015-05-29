9d8d3dc52f0afb2d7ab7e3c4cdf11cbced80df16
te85kibe <thomaskoster@Thomass-MacBook-Air.local>
2015-05-29 13:59:05 +0200

    residual geht nicht runter

--- src/Smoother2.cc
+++ src/Smoother2.cc
@@ -28,11 +28,11 @@ void Smoother::smooth_red_black_gauss_seidel_2d ( Array & u,    // modify this a
 	real h_2     = h * h;
 	real h_2_inv = 1.0 / h_2;
 	real factor  = h_2 * 0.25;
-
+	
 	for (int iter = 0; iter < times; iter++)
 	{
 		// red points
-		for (int j = 1; j < height-1; j++)
+		for (int j = 1; j < (height/2); j++)
 		{
 			for (int i = 1; i < width-1; i++)
 			{
@@ -42,9 +42,30 @@ void Smoother::smooth_red_black_gauss_seidel_2d ( Array & u,    // modify this a
 						u(i,j) = factor * (f(i,j) + h_2_inv * ( u(i-1, j) + u(i+1, j) + u(i, j+1) + u(i, j-1)));
 			}
 		}
+		for (int j = (height/2)+1; j < height-1; j++)
+		{
+			for (int i = 1; i < width-1; i++)
+			{
+					// inner domain
+					// i+j gerade
+					if( ((i + j) % 2) == 0)
+						u(i,j) = factor * (f(i,j) + h_2_inv * ( u(i-1, j) + u(i+1, j) + u(i, j+1) + u(i, j-1)));
+			}
+		}
+		
+		for (int i = 1; i < (width/2); i++)
+		{
+			// inner domain
+			// i+j gerade
+			int j = height/2;
+			if( ((i + j) % 2) == 0)
+				u(i,j) = factor * (f(i,j) + h_2_inv * ( u(i-1, j) + u(i+1, j) + u(i, j+1) + u(i, j-1)));
+		}
+
+
 
 		// black points
-		for (int j = 1; j < height-1; j++)
+		for (int j = 1; j < (height/2); j++)
 		{
 			for (int i = 1; i < width-1; i++)
 			{
@@ -54,6 +75,25 @@ void Smoother::smooth_red_black_gauss_seidel_2d ( Array & u,    // modify this a
 						u(i,j) = factor * (f(i,j) + h_2_inv * ( u(i-1, j) + u(i+1, j) + u(i, j+1) + u(i, j-1)));
 			}
 		}
+		for (int j = (height/2)+1; j < height-1; j++)
+		{
+			for (int i = 1; i < width-1; i++)
+			{
+					// inner domain
+					// i+j ungerade
+					if( ((i + j) % 2) == 1)
+						u(i,j) = factor * (f(i,j) + h_2_inv * ( u(i-1, j) + u(i+1, j) + u(i, j+1) + u(i, j-1)));
+			}
+		}
+		for (int i = 1; i < (width/2); i++)
+		{
+			// inner domain
+			// i+j ungerade
+			int j = height/2;
+			if( ((i + j) % 2) == 1)
+				u(i,j) = factor * (f(i,j) + h_2_inv * ( u(i-1, j) + u(i+1, j) + u(i, j+1) + u(i, j-1)));
+		}
+
 	}
 }
 
